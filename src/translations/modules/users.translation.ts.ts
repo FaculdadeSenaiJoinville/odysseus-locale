a926bfd11c1ceaae2aaae7a92e3842cbab1e93b7
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-08-20 21:20:50 -0300

    feature(users): adding new translations #PES-240

--- src/translations/modules/users.translation.ts.ts
+++ src/translations/modules/users.translation.ts.ts
@@ -13,6 +13,10 @@ export class Users extends Locale {
 
 			confirm_password: 'Confirmar Senha',
 
+			new_password: 'Nova Senha',
+
+			confirm_new_password: 'Confirmar Nova Senha',
+
 			active: 'Ativo',
 
 			type: 'Tipo'
@@ -45,7 +49,9 @@ export class Users extends Locale {
 		};
 
 		this.labels = {
-			create_new_user: 'Cadastrar um novo usuário'
+			create_new_user: 'Cadastrar um novo usuário',
+
+			save_password: 'Salvar Senha'
 		}
 	}
 
