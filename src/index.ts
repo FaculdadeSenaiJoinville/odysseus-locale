a989b1870d7b742cbfb5c8ed5ecab1f9cf1b6513
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-09-29 22:18:12 -0300

    feature(groups): creating groups translations #PES-311

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import {
 	Auth,
 	Joi,
 	SystemErrors,
-	Users
+	Users,
+	Groups
 } from './translations';
 import { Misc } from './core/misc';
 
@@ -11,13 +12,15 @@ export class OdysseyDictionary {
 	errors: SystemErrors;
 	auth: Auth;
 	users: Users;
+	groups: Groups;
 }
 
 export const Dictionary: OdysseyDictionary = {
 	misc: new Misc(),
 	errors: new SystemErrors(),
 	auth: new Auth(),
-	users: new Users()
+	users: new Users(),
+	groups: new Groups()
 };
 
 export const JoiMessages = new Joi();
